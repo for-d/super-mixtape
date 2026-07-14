1b0df269d2740464996321a5236ad65913f445d5
for-d <floresbconstanza@gmail.com>
2026-07-13 21:15:37 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("Super Mixtape iniciado.");
+const jewelCase = document.getElementById("jewelCase");
+
+document.addEventListener("mousemove",(e)=>{
+
+const x =
+(e.clientX/window.innerWidth -0.5)*20;
+
+const y =
+(e.clientY/window.innerHeight -0.5)*20;
+
+jewelCase.style.transform =
+`rotateY(${x}deg) rotateX(${-y}deg)`;
+
+});
