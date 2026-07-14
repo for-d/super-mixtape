ad6eef7ae1800a4d5c3a59d6e4354f1e69ce45d7
for-d <floresbconstanza@gmail.com>
2026-07-13 23:37:58 -0400

    Update disc.js

--- js/disc.js
+++ js/disc.js
@@ -1,1 +1,21 @@
+const disc=document.querySelector(".disc");
 
+let discOutside=false;
+
+disc.addEventListener("dblclick",()=>{
+
+discOutside=!discOutside;
+
+if(discOutside){
+
+disc.classList.add("removed");
+
+}
+
+else{
+
+disc.classList.remove("removed");
+
+}
+
+});
