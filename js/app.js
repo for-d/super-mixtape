c5e7e2b1ae5008560036386860f145a770992152
for-d <floresbconstanza@gmail.com>
2026-07-14 01:17:09 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
+const caja=document.querySelector(".case");
 
+let abierta=false;
+
+caja.addEventListener("click",()=>{
+
+abierta=!abierta;
+
+if(abierta){
+
+caja.classList.add("open");
+
+}else{
+
+caja.classList.remove("open");
+
+}
+
+});
+
+document.addEventListener("mousemove",(e)=>{
+
+const x=(e.clientX/window.innerWidth-.5)*12;
+
+const y=(e.clientY/window.innerHeight-.5)*-12;
+
+caja.style.transform=
+
+`rotateY(${x}deg) rotateX(${y}deg)`;
+
+});
