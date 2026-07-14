f56c389d6071dbaafd9e257d5debf9782a93275c
for-d <floresbconstanza@gmail.com>
2026-07-14 01:21:58 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const caja=document.querySelector(".case");
 
+const disco=document.querySelector(".disc");
+
 let abierta=false;
 
-caja.addEventListener("click",()=>{
+let discoFuera=false;
+
+caja.addEventListener("click",(e)=>{
+
+if(e.target===disco)return;
 
 abierta=!abierta;
 
@@ -13,6 +19,30 @@ caja.classList.add("open");
 }else{
 
 caja.classList.remove("open");
+
+disco.classList.remove("outside");
+
+discoFuera=false;
+
+}
+
+});
+
+disco.addEventListener("click",(e)=>{
+
+e.stopPropagation();
+
+if(!abierta)return;
+
+discoFuera=!discoFuera;
+
+if(discoFuera){
+
+disco.classList.add("outside");
+
+}else{
+
+disco.classList.remove("outside");
 
 }
 
