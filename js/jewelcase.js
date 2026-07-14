e151aee3078fcc0f4323818bc24cd1e4d4123acf
for-d <floresbconstanza@gmail.com>
2026-07-13 22:17:12 -0400

    Update jewelcase.js

--- js/jewelcase.js
+++ js/jewelcase.js
@@ -1,1 +1,17 @@
 console.log("Jewel Case Ready");
+
+transform-style:preserve-3d;
+
+cursor:pointer;
+
+transform:
+
+rotateX(8deg)
+
+rotateY(-14deg);
+
+transition:
+
+transform .6s,
+
+box-shadow .6s;
