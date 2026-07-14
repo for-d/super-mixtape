9ea211873b1204ab264d1ed38531b1b8a4bcda30
for-d <floresbconstanza@gmail.com>
2026-07-13 22:35:07 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const jewel = document.querySelector(".jewel-case");
+const lid = document.querySelector(".lid");
+
+let open = false;
 
 document.addEventListener("mousemove",(e)=>{
 
@@ -6,12 +9,29 @@ const x=(window.innerWidth/2-e.clientX)/35;
 
 const y=(window.innerHeight/2-e.clientY)/35;
 
+if(!open){
+
 jewel.style.transform=
 
 `rotateX(${y}deg)
+ rotateY(${-x}deg)`;
 
-rotateY(${-x}deg)
+}
 
-`;
+});
+
+jewel.addEventListener("click",()=>{
+
+open=!open;
+
+if(open){
+
+lid.style.transform="rotateY(-120deg)";
+
+}else{
+
+lid.style.transform="rotateY(0deg)";
+
+}
 
 });
